606d9c46021ab3078ec1874f4a236252b7f91c72
Paul Yarin <pavyarin@yandex.ru>
2021-11-29 00:01:54 +0700

    Отрисована шапка приложения

--- src/main.js
+++ src/main.js
@@ -1,3 +1,6 @@
+import {createMainMenuTemplate} from "./view/main-menu.js";
+import {createTripInfoTemplate} from "./view/trip-info.js";
+
 const RenderPosition = {
   BEFOREBEGIN: 'beforebegin',
   AFTERBEGIN: 'afterbegin',
@@ -8,3 +11,10 @@ const RenderPosition = {
 const renderTemplate = (container, template, place) => {
   container.insertAdjacentHTML(place, template);
 };
+
+const siteHeader = document.querySelector('.page-header');
+const mainTripInfoContainer = siteHeader.querySelector('.trip-main');
+const menuContainer = mainTripInfoContainer.querySelector('.trip-controls__navigation');
+
+renderTemplate(mainTripInfoContainer, createTripInfoTemplate(), RenderPosition.AFTERBEGIN);
+renderTemplate(menuContainer, createMainMenuTemplate(), RenderPosition.BEFOREEND);
