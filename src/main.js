bc20f6fedb6ea92baff59071d73a2172b74dc153
Paul Yarin <pavyarin@yandex.ru>
2021-11-29 00:28:11 +0700

    Отрисованы все необходимые компоненты на всю страницу приложения

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
-import {createMainMenuTemplate} from "./view/main-menu.js";
-import {createTripInfoTemplate} from "./view/trip-info.js";
+import {createMainMenuTemplate} from './view/main-menu.js';
+import {createTripInfoTemplate} from './view/trip-info.js';
+import {createFilterTemplate} from './view/filter-events.js';
+import {createSortOffersTemplate} from './view/sort-events.js';
+import {createEditEventTemplate} from './view/edit-event.js';
+import {createEventTemplate} from './view/event.js';
 
 const RenderPosition = {
   BEFOREBEGIN: 'beforebegin',
@@ -8,6 +12,8 @@ const RenderPosition = {
   AFTEREND: 'afterend',
 };
 
+const EVENT_COUNT = 3;
+
 const renderTemplate = (container, template, place) => {
   container.insertAdjacentHTML(place, template);
 };
@@ -15,6 +21,20 @@ const renderTemplate = (container, template, place) => {
 const siteHeader = document.querySelector('.page-header');
 const mainTripInfoContainer = siteHeader.querySelector('.trip-main');
 const menuContainer = mainTripInfoContainer.querySelector('.trip-controls__navigation');
+const filterContainer = mainTripInfoContainer.querySelector('.trip-controls__filters');
+const mainContent = document.querySelector('.page-main');
+const sortContainer = mainContent.querySelector('.trip-events');
 
 renderTemplate(mainTripInfoContainer, createTripInfoTemplate(), RenderPosition.AFTERBEGIN);
 renderTemplate(menuContainer, createMainMenuTemplate(), RenderPosition.BEFOREEND);
+renderTemplate(filterContainer, createFilterTemplate(), RenderPosition.BEFOREEND);
+renderTemplate(sortContainer, createSortOffersTemplate(), RenderPosition.BEFOREEND);
+
+const eventsContainer = mainContent.querySelector('.trip-events__list');
+renderTemplate(eventsContainer, createEditEventTemplate(), RenderPosition.BEFOREEND);
+
+for (let i = 0; i < EVENT_COUNT; i++) {
+  renderTemplate(eventsContainer, createEventTemplate(), RenderPosition.BEFOREEND);
+}
+
+
