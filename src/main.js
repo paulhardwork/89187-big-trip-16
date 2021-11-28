b16f8a7ff7f94616de0eb32aec2a74fcbed58317
Paul Yarin <pavyarin@yandex.ru>
2021-11-28 18:09:17 +0700

    Создана функция для отрисовки разметки

---
+++ src/main.js
@@ -0,0 +1,10 @@
+const RenderPosition = {
+  BEFOREBEGIN: 'beforebegin',
+  AFTERBEGIN: 'afterbegin',
+  BEFOREEND: 'beforeend',
+  AFTEREND: 'afterend',
+};
+
+const renderTemplate = (container, template, place) => {
+  container.insertAdjacentHTML(place, template);
+};
