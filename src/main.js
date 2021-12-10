079b06247315ff4d067ee215a616465c7053c118
Paul Yarin <pavyarin@yandex.ru>
2021-12-10 17:58:13 +0700

    Подготовлена отрисовка точек маршрута

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import {createFilterTemplate} from './view/filter-events.js';
 import {createSortOffersTemplate} from './view/sort-events.js';
 import {createEditEventTemplate} from './view/edit-event.js';
 import {createEventTemplate} from './view/event.js';
+import {generatePoint} from './mock/point.js';
 
 const RenderPosition = {
   BEFOREBEGIN: 'beforebegin',
@@ -17,6 +18,8 @@ const EVENT_COUNT = 3;
 const renderTemplate = (container, template, place) => {
   container.insertAdjacentHTML(place, template);
 };
+
+const points = Array.from({length: EVENT_COUNT}, generatePoint);
 
 const siteHeader = document.querySelector('.page-header');
 const mainTripInfoContainer = siteHeader.querySelector('.trip-main');
@@ -34,7 +37,6 @@ const eventsContainer = mainContent.querySelector('.trip-events__list');
 renderTemplate(eventsContainer, createEditEventTemplate(), RenderPosition.BEFOREEND);
 
 for (let i = 0; i < EVENT_COUNT; i++) {
-  renderTemplate(eventsContainer, createEventTemplate(), RenderPosition.BEFOREEND);
+  renderTemplate(eventsContainer, createEventTemplate(points[i]), RenderPosition.BEFOREEND);
 }
-
 
