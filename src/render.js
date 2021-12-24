68baa21fade21f6798997d1ae8af97303b5f14ec
Paul Yarin <pavyarin@yandex.ru>
2021-12-24 17:32:21 +0700

    Поправлена функция создания элемента

--- src/render.js
+++ src/render.js
@@ -28,7 +28,7 @@ export const render = (container, element, place) => {
 
 export const createElement = (template) => {
   const element = document.createElement('div');
-  element.innerHTML(template);
+  element.innerHTML = template;
 
   return element.firstChild;
 };
