5c4ea071f44958ff01f784c3ad4c8377b1cde6e6
Paul Yarin <pavyarin@yandex.ru>
2021-12-24 17:13:55 +0700

    Созданы функции для создания элементов и их рендера

---
+++ src/render.js
@@ -0,0 +1,36 @@
+export const RenderPosition = {
+  BEFOREBEGIN: 'beforebegin',
+  AFTERBEGIN: 'afterbegin',
+  BEFOREEND: 'beforeend',
+  AFTEREND: 'afterend',
+};
+
+export const renderTemplate = (container, template, place) => {
+  container.insertAdjacentHTML(place, template);
+};
+
+export const render = (container, element, place) => {
+  switch (place) {
+    case RenderPosition.BEFOREBEGIN:
+      container.before(element);
+      break;
+    case RenderPosition.AFTERBEGIN:
+      container.prepend(element);
+      break;
+    case RenderPosition.BEFOREEND:
+      container.append(element);
+      break;
+    case RenderPosition.AFTEREND:
+      container.after(element);
+      break;
+  }
+};
+
+export const createElement = (template) => {
+  const element = document.createElement('div');
+  element.innerHTML(template);
+
+  return element.firstChild;
+};
+
+
