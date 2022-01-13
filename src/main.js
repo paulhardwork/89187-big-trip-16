dd4e52949c5bf0cce88503d6b1ec68ec1f3975cc
Paul Yarin <pavyarin@yandex.ru>
2022-01-13 23:51:44 +0700

    Перенесена реализация отрисовки страницы на презентер

--- src/main.js
+++ src/main.js
@@ -1,30 +1,12 @@
 import {generatePoint} from './mock/point.js';
-import {RenderPosition, render, replace} from './render.js';
-import PointsContainer from './view/points-container.js';
-import SortPoints from './view/sort-points.js';
-import Point from './view/point.js';
-import EditPoint from './view/edit-point.js';
+import {RenderPosition, render} from './render.js';
 import MainMenu from './view/main-menu.js';
 import FilterPoints from './view/filter-points.js';
+import TripPresenter from './presenter/trip-presenter.js';
 
 const EVENT_COUNT = 20;
 
 const points = Array.from({length: EVENT_COUNT}, generatePoint);
-
-const renderPoint = (container, point) => {
-  const pointComponent = new Point(point);
-  const editPointComponent = new EditPoint(point);
-
-  pointComponent.setEditClickHandler(() => {
-    replace(editPointComponent, pointComponent);
-  });
-
-  editPointComponent.setFormSubmitHandler(() => {
-    replace(pointComponent, editPointComponent);
-  });
-
-  render(container, pointComponent, RenderPosition.BEFOREEND);
-};
 
 const siteHeader = document.querySelector('.page-header');
 const mainTripInfoContainer = siteHeader.querySelector('.trip-main');
@@ -32,15 +14,9 @@ const menuContainer = mainTripInfoContainer.querySelector('.trip-controls__navig
 const filterContainer = mainTripInfoContainer.querySelector('.trip-controls__filters');
 const mainContent = document.querySelector('.page-main');
 const tripListContainer = mainContent.querySelector('.trip-events');
+const tripPresenter = new TripPresenter(tripListContainer);
 
 render(menuContainer, new MainMenu(), RenderPosition.BEFOREEND);
 render(filterContainer, new FilterPoints(), RenderPosition.BEFOREEND);
-render(tripListContainer, new SortPoints(), RenderPosition.BEFOREEND);
-render(tripListContainer, new PointsContainer(), RenderPosition.BEFOREEND);
 
-const pointsContainer = mainContent.querySelector('.trip-events__list');
-
-for (let i = 0; i < EVENT_COUNT; i++) {
-  renderPoint(pointsContainer, points[i]);
-}
-
+tripPresenter.init(points);
