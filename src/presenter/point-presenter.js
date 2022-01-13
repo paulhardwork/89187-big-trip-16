0db3eff3eb394ee6a0be851d9ad876465579bbec
Paul Yarin <pavyarin@yandex.ru>
2022-01-14 00:05:28 +0700

    Создан отдельный презентер для точки маршрута

---
+++ src/presenter/point-presenter.js
@@ -0,0 +1,52 @@
+import Point from '../view/point.js';
+import EditPoint from '../view/edit-point.js';
+import {render, RenderPosition, replace} from '../utils/render.js';
+
+export default class PointPresenter {
+  #pointsListContainer = null;
+  #pointComponent = null;
+  #editPointComponent = null;
+
+  #point = null;
+
+  constructor(pointsListContainer) {
+    this.#pointsListContainer = pointsListContainer;
+  }
+
+  init = (point) => {
+    this.#point = point;
+
+    this.#pointComponent = new Point(point);
+    this.#editPointComponent = new EditPoint(point);
+
+    this.#pointComponent.setEditClickHandler(this.#handleEditClick);
+    this.#editPointComponent.setFormSubmitHandler(this.#handleFormSubmit);
+
+    render(this.#pointsListContainer, this.#pointComponent, RenderPosition.BEFOREEND);
+  }
+
+  #replacePointToForm = () => {
+    replace(this.#editPointComponent, this.#pointComponent);
+    document.addEventListener('keydown', this.#escKeyDownHandler);
+  }
+
+  #replaceFormToPoint = () => {
+    replace(this.#pointComponent, this.#editPointComponent);
+    document.removeEventListener('keydown', this.#escKeyDownHandler);
+  }
+
+  #escKeyDownHandler = (evt) => {
+    if (evt.key === 'Escape' || evt.key === 'Esc') {
+      evt.preventDefault();
+      this.#replaceFormToPoint();
+    }
+  }
+
+  #handleEditClick = () => {
+    this.#replacePointToForm();
+  }
+
+  #handleFormSubmit = () => {
+    this.#replaceFormToPoint();
+  }
+}
